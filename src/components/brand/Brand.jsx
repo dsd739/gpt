9c5809ee1cf81499f93df5a69a75ac788de1e790
dsd739 <durgadangi.2021cs@technonjr.org>
2023-03-05 22:14:29 +0530

    added Brand component with responsive property  Changes to be committed: 	modified:   src/components/brand/Brand.jsx 	modified:   src/components/brand/brand.css

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './brand.css'
+import image_1 from '../../assets/google.png'
+import image_2 from '../../assets/slack.png'
+import image_3 from '../../assets/atlassian.png'
+import image_4 from '../../assets/dropbox.png'
+import image_5 from '../../assets/shopify.png'
 
 export default function Brand() {
   return (
-    <div>Brand</div>
+    <div>
+      <div className="brand_container">
+          <img src={image_1} alt="Google" />
+          <img src={image_2} alt="Slack" />
+          <img src={image_3} alt="Atlassian" />
+          <img src={image_4} alt="Dropbox" />
+          <img src={image_5} alt="Shopify" />
+      </div>
+
+    </div>
   )
 }
