96714e024e142a6cdd3808c5baa96ca9e90c7b02
dsd739 <durgadangi.2021cs@technonjr.org>
2023-03-05 21:53:43 +0530

    created a header component with full responsivnes  Changes to be committed: 	modified:   src/App.js 	modified:   src/container/header/Header.jsx 	modified:   src/container/header/header.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Article,Brand,Cta, Navbar } from './components';
 function App() {
   return (
     <div className='App  gradient__bg'>
-      <div className='gradient_bg'>
+      <div >
       <Navbar/>
       <Header/>
       </div>
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import image_2 from "../../assets/people.png";
+import image_1 from '../../assets/ai.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header1">
+      <div className="header">
+        <div className="header__heading gradient__text">
+          <p>Let’s Build Something amazing with GPT-3 OpenAI</p>
+        </div>
+        <div className="header__discription">
+          <p>
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
+        </div>
+        <div className="header__email">
+          <input
+            className="header__email-inpt"
+            type="email"
+            placeholder="Your Email Address"
+          />
+          <button className="header__email-btn" type="submit">
+            Get Started
+          </button>
+        </div>
+        <div className="header__email-request_forms">
+          <img src={image_2} />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="header_image">
+        <img src={image_1} alt="header_image" />
+      </div>
+    </div>
+  );
 }
